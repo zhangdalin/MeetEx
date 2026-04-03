a248960a6acafc17fff87c11c668cc857006ec24
Darin Zhang <512776357@qq.com>
2026-04-03 21:19:07 +0800

    修复：添加 mediaengine 库链接和翻译文件内容

--- i18n/MeetEx_zh_CN.ts
+++ i18n/MeetEx_zh_CN.ts
@@ -1,3 +1,65 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+  <context>
+    <name>Login</name>
+    <message>
+      <location filename="../ui/login.ui" line="15"/>
+      <source>登录</source>
+      <translation>登录</translation>
+    </message>
+    <message>
+      <location filename="../ui/login.ui" line="30"/>
+      <source>手机号：</source>
+      <translation>手机号：</translation>
+    </message>
+    <message>
+      <location filename="../ui/login.ui" line="51"/>
+      <source>用户名：</source>
+      <translation>用户名：</translation>
+    </message>
+    <message>
+      <location filename="../ui/login.ui" line="72"/>
+      <source>密    码：</source>
+      <translation>密    码：</translation>
+    </message>
+    <message>
+      <location filename="../ui/login.ui" line="88"/>
+      <source>登录</source>
+      <translation>登录</translation>
+    </message>
+  </context>
+  <context>
+    <name>Home</name>
+    <message>
+      <location filename="../ui/home.ui" line="15"/>
+      <source>主页</source>
+      <translation>主页</translation>
+    </message>
+    <message>
+      <location filename="../ui/home.ui" line="98"/>
+      <source>头像</source>
+      <translation>头像</translation>
+    </message>
+    <message>
+      <location filename="../ui/home.ui" line="102"/>
+      <source>会议</source>
+      <translation>会议</translation>
+    </message>
+    <message>
+      <location filename="../ui/home.ui" line="120"/>
+      <source>加入会议</source>
+      <translation>加入会议</translation>
+    </message>
+    <message>
+      <location filename="../ui/home.ui" line="130"/>
+      <source>快速会议</source>
+      <translation>快速会议</translation>
+    </message>
+    <message>
+      <location filename="../ui/home.ui" line="140"/>
+      <source>预约会议</source>
+      <translation>预约会议</translation>
+    </message>
+  </context>
+</TS>
