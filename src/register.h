2327cbf33b607c098ece60a3a29ec3c282957db6
Darin Zhang <512776357@qq.com>
2026-05-31 15:56:26 +0800

    feat(ui): add avatarPreviewLabel member and onAvatarUrlChanged slot to Register class

--- src/register.h
+++ src/register.h
@@ -6,6 +6,7 @@
 #include "services/auth_service.h"
 
 QT_BEGIN_NAMESPACE
+class QLabel;
 namespace Ui { class Register; }
 QT_END_NAMESPACE
 
@@ -29,6 +30,7 @@ private slots:
     void onRegisterFailed(const QString &error);
     void onCodeSent();
     void onCodeSendFailed(const QString &error);
+    void onAvatarUrlChanged(const QString &url);
 
 private:
     bool validateAccount();
@@ -41,6 +43,7 @@ private:
     QTimer *countdownTimer_;
     int countdown_;
     bool registered_ = false;
+    QLabel *avatarPreviewLabel_ = nullptr;
 };
 
 #endif // REGISTER_H
