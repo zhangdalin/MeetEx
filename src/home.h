c2dba2a2350d3274fa9e6aca4abdd345e669b466
Darin Zhang <512776357@qq.com>
2026-05-31 18:04:20 +0800

    feat(home): 添加会议列表数据结构和相关方法声明

--- src/home.h
+++ src/home.h
@@ -2,6 +2,8 @@
 #define HOME_H
 
 #include <QWidget>
+#include <QDateTime>
+#include <QListWidgetItem>
 #include "services/auth_service.h"
 
 using namespace std;
@@ -9,6 +11,20 @@ using namespace std;
 namespace Ui {
 class Home;
 }
+
+enum class MeetingStatus {
+    Pending,    // 待开始
+    Active,     // 进行中
+    Ended       // 已结束
+};
+
+struct MeetingInfo {
+    QString meetingId;      // 会议号
+    QString topic;          // 会议主题
+    QDateTime startTime;    // 开始时间
+    QDateTime endTime;      // 结束时间
+    MeetingStatus status;   // 状态
+};
 
 class Home : public QWidget
 {
@@ -38,8 +54,19 @@ private slots:
     void onSettingsBtn();
     void onLogoutBtn();
 
+    // Meeting list slots
+    void onMeetingItemClicked(QListWidgetItem *item);
+    void onJoinMeetingFromList(const QString &meetingId);
+
 private:
     Ui::Home *ui;
+
+    // Meeting list methods
+    void loadMeetingList();
+    void addMeetingItem(const MeetingInfo &info);
+    QString formatMeetingTime(const QDateTime &start, const QDateTime &end);
+    QString getStatusText(MeetingStatus status);
+    QString getStatusStyle(MeetingStatus status);
 };
 
 #endif // HOME_H
